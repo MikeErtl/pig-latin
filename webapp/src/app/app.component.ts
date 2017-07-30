ad4aef94d0fa8679241784d4f573f67cd406da11
Mike Ertl <16319002+MikeErtl@users.noreply.github.com>
2017-07-30 13:19:57 +0100

    Bootstrap user interface added

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,7 +1,22 @@
 import { Component } from '@angular/core';
 
+export class Translation {
+  english: string;
+  pigLatin: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/app.component.html'
 })
-export class AppComponent  { name = 'Pig Latin Converter'; }
+
+export class AppComponent  { 
+  title = 'Pig Latin Converter';
+  enterEnglishInstruct = 'English:'; 
+  pigLatinInstruct = 'Pig Latin:'
+  translation: Translation = {
+      english: '',
+      pigLatin: ''
+  };
+}
+
